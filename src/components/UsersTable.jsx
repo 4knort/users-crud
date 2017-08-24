a3c34a415f77cc4925a855e33ce20d9c6e7912c9
Nort <4knort@gmail.com>
2017-08-24 16:06:13 +0300

    rolled back before chrome bug fixing

--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -2,32 +2,29 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const UsersTable = ({ users, onClick }) => {
-  let usersTable = null;
-  if (users) {
-
-    usersTable = users.map((user, index) => {
-      const date = `${user.date.day}.${user.date.month}.${user.date.year}`;   
-      if (user) {
-        return (
-          <tr key={`user-${index}`}>
-            <td>{user.name}</td>
-            <td>{date}</td>
-            <td>{user.city}</td>
-            <td>{user.adress}</td>
-            <td>{user.phone}</td>
-            <td>
-              <button onClick={() => onClick(user.id)}>delete</button>
-            </td>
-            <td>
-              <Link to={`/user/${user.id}`}>Update</Link>
-            </td>
-          </tr>
-        );
-      } else {
-        return null;
-      }
-    });
-  }
+  const usersTable = users.map((user, index) => {
+    const date = `${user.date.day}.${user.date.month}.${user.date.year}`;
+    
+    if (user) {
+      return (
+        <tr key={`user-${index}`}>
+          <td>{user.name}</td>
+          <td>{date}</td>
+          <td>{user.city}</td>
+          <td>{user.adress}</td>
+          <td>{user.phone}</td>
+          <td>
+            <button onClick={() => onClick(user.id)}>delete</button>
+          </td>
+          <td>
+            <Link to={`/user/${user.id}`}>Update</Link>
+          </td>
+        </tr>
+      );
+    } else {
+      return null;
+    }
+  });
 
   return (
     <table>
